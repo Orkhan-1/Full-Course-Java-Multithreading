7a13f310483c37d911de85cf4059f0a1b68581a4
Orkhan <gasanov.orkhan1@gmail.com>
2025-08-21 02:58:42 -0700

    [Tutorial 8] Locks (ReentrantLock, ReadWriteLock)

---
+++ ReentrantLockDemo.java
@@ -0,0 +1,29 @@
+import java.util.concurrent.locks.*;
+
+public class ReentrantLockDemo {
+    private static int counter = 0;
+    private static Lock lock = new ReentrantLock();
+
+    public static void increment() {
+        lock.lock();
+        try {
+            counter++;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Runnable task = () -> {
+            for (int i = 0; i < 1000; i++) increment();
+        };
+
+        Thread t1 = new Thread(task);
+        Thread t2 = new Thread(task);
+
+        t1.start(); t2.start();
+        t1.join(); t2.join();
+
+        System.out.println("Final counter value: " + counter);
+    }
+}
