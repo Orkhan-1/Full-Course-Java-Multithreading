94d264f32928e59f0ee927efb8ef95e313f52f77
Orkhan <gasanov.orkhan1@gmail.com>
2025-08-21 02:55:42 -0700

    [Tutorial 7] synchronized Keyword – Methods & Blocks

---
+++ SynchronizedDemo.java
@@ -0,0 +1,25 @@
+public class SynchronizedDemo {
+    private static int counter = 0;
+
+    // synchronized method
+    public static synchronized void increment() {
+        counter++;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Runnable task = () -> {
+            for (int i = 0; i < 1000; i++) {
+                increment();
+            }
+        };
+
+        Thread t1 = new Thread(task);
+        Thread t2 = new Thread(task);
+
+        t1.start(); t2.start();
+        t1.join(); t2.join();
+
+        System.out.println("Final counter value: " + counter);
+    }
+}
+// Expected value = 2000, and actual value will always be 2000 due to synchronization
